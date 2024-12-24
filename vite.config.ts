2afe8cc42c5634845cabd0051799b7dd1fe94df7
glunoty <iluviums@gmail.com>
2024-12-25 00:21:20 +0700

    using vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    Sitemap({ hostname: "https://tracylio.vercel.app" }),
+  ],
 });
